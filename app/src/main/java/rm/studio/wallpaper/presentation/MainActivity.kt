8dd09183290b85668692d50fbe3c6e485e4552e0
Rustamjon Mullaqo'ziyev <mullaqoziyev.rustamjon@gmail.com>
2022-10-15 16:29:09 +0500

    first comment

---
+++ app/src/main/java/rm/studio/wallpaper/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package rm.studio.wallpaper.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import rm.studio.wallpaper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
